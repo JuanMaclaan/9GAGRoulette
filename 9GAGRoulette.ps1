15c3e03c0ce161946695b14ccbbc61a1df2b1bfb
schokapyk <42220039+schokapyk@users.noreply.github.com>
2020-02-15 20:53:46 +0100

    Add files via upload

--- 9GAGRoulette.ps1
+++ 9GAGRoulette.ps1
@@ -4,31 +4,77 @@
 # DATE: 19/10/2019
 #
 #
-# VERSION 1.0
+# VERSION 1.1
 # COMMENTS: Access to random post/section from 9gag.com
-#
+# SPECIAL THANKS TO : https://github.com/Hei5enberg44
 # =======================================================
 
+# =======================================================
+#                       Variables
+# =======================================================
+$9sec = @("france", "funny", "animals", "anime-manga", "animewaifu", "awesome", "car", "comic-webtoon", "cosplay", "gaming", "girl", "girlcelebrity", "leagueoflegends", "meme", "nsfw", "politics", "relationship", "savage", "wtf", "animewallpaper", "apexlegends", "ask9gag", "countryballs", "home-living", "crappydesign", "drawing-diy-crafts", "food-drinks", "football", "fortnite", "got", "guy", "history", "horror", "kpop", "timely", "lego", "superhero", "movie-tv", "music", "basketball", "overwatch", "pcmr", "pokemon", "pubg", "satisfying", "science-tech", "sport", "starwars", "school", "rate-my-outfit", "travel-photography", "wallpaper", "warhammer", "wholesome", "darkhumor")
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+$file = "$($ScriptDir)\9GAGRoulette.ps1"
+$script:i = 0
+$script:o = 0
+# ======================================================
+#                       Functions
+# ======================================================
 
-##Random 9gag
+## For that function use 2 parameters, the line you want to replace in the script and the content you want to use to replace the line
+Function majsections ($find) {
 
-##Variables
+$findline = $((Get-Content $file)[$find])
+(Get-Content $file).replace($findline,$($findline.substring(0,10)+$sections+")")) | Set-Content $file
+$script:i++
+}
 
-$9gag = invoke-webrequest "https://9gag.com/" -UseBasicParsing
+##Template
+Function majscript($find,$replace) {
 
-##Function
+$findline = $((Get-Content $file)[$find])
+(Get-Content $file).replace($findline,$replace) | Set-Content $file
+$i++
+}
+
 
 ##Function randompost (9gag have already a link to do that)
 Function randompost {  Start-Process -FilePath "http://bit.ly/3231YVs"}
 
 ##Function randomsection
 Function randomsection {
-	$randomsec = Get-Random ($9gag.AllElements | Where {$_.class -eq "badge-upload-section-list-item-selector selector"} | select data-url)
-	Start-Process -FilePath "https://9gag.com/$($randomsec."data-url")"
+    
+    #If the check box "Update Section List" is check and the var $script:o is not -eq to 0, it will request the section list from 9gag.com, else it use $9sec
+    if ($checkbox1.checked -and $script:o -eq 0) {
+        $9gag = invoke-webrequest "https://9gag.com/"
+                if ($9gag -is [int]) {
+                     $gag = invoke-webrequest "https://9gag.com/" -UseBasicParsing
+                }
+                $9gagsec = $9gag.AllElements | Where {$_.class -eq "badge-upload-section-list-item-selector selector"} | select data-url | ForEach { $_."data-url" }
+                    $array = @()
+	                Foreach ($sec in $9gagsec) {
+                    $array += """" + $sec + """"
+                    }
+                    $sections = [system.String]::Join(", ", $array)
+                    $script:o++           
+    }
+                     
+    Else{
+        $9gagsec = $9sec
+    }
+        
+        $randomsec = $9gagsec | get-random
+        
+        Start-Process -FilePath "https://9gag.com/$($randomsec)"
+     #This part update the section list in the script file
+     if ($checkbox1.checked) {
+        if ($script:i -eq 0){
+        majsections 14
+        }
+     }
+
 }
-
 ##Function randompopular
-
 Function randompopular {
 
 	$num = Get-Random -minimum 1 -maximum 4
@@ -61,7 +107,6 @@ function Hide-Console
 ##Function GUI 
 
 function GUI {
-
 
  Add-Type -AssemblyName System.Windows.Forms
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Form")
@@ -105,6 +150,12 @@ $Post.Font = 'Segoe UI,13,style=Bold'
 $Post.ForeColor = "#ffffff"
 $Post.Add_Click({randompost})
 $9GAGRoulette.Controls.add($Post)
+
+$checkbox1 = new-object System.Windows.Forms.checkbox
+$checkbox1.Location = new-object System.Drawing.Size(40,0)
+$checkbox1.Text = "Update Section"
+$checkbox1.Checked = $false
+$9GAGRoulette.Controls.Add($checkbox1)
 
 ##Show Form
 $9GAGRoulette.Add_Shown({
